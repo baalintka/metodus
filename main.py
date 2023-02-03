f5d22ee950fbc08b9357e0a6bed4fa8201a3db58
baalintka <ballint3@gmail.com>
2023-02-03 13:30:56 +0100

    q

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def eredmeny(Jatekospontok:[int], Geppontok:[int]):
         szoveg = "jatekosvesztett"
     elif Geppontok >21:
         szoveg = "gepvesztett"
-    elif Jatekospontok<Geppontok:
+    elif Jatekospontok < Geppontok:
         szoveg = "jatekosvesztett"
+    elif Jatekospontok==Geppontok:
+        szoveg= "döntetlen"
+
     return szoveg
 def pontszamitas(lapok: [int]):
     pontok=0
@@ -29,7 +32,7 @@ def jatekosVesztettTeszt():
        print("Teszt sikeres")
    else:
        print("Teszt megbukott")
-def jatekosVesztettTeszt2():
+def jatekosVesztettkevesebbponttalTeszt():
    jatekoslista = [6, 4, 8,]
    geplista =[6, 4, 11]
    kapotteredmeny = eredmeny(jatekoslista,geplista)
@@ -38,8 +41,62 @@ def jatekosVesztettTeszt2():
        print("Teszt sikeres2")
    else:
        print("Teszt megbukott2")
+
+def jatekosVesztetttobblappalTeszt():
+   jatekoslista = [6, 4, 8, 3]
+   geplista =[6, 4, 11]
+   kapotteredmeny = eredmeny(jatekoslista,geplista)
+   varteredmeny = "jatekosvesztett"
+   if kapotteredmeny==varteredmeny:
+       print("Teszt sikeres")
+   else:
+       print("Teszt megbukott")
+
+def gepVesztettTeszt():
+   jatekoslista = [3,6,5]
+   geplista =[9,8,6]
+   kapotteredmeny = eredmeny(jatekoslista,geplista)
+   varteredmeny = "jatekosvesztett"
+   if kapotteredmeny==varteredmeny:
+       print("Teszt sikeres")
+   else:
+       print("Teszt megbukott")
+
+def gepVesztettkevesebbponttalTeszt():
+   jatekoslista = [3, 6, 5, 5]
+   geplista = [9, 8,]
+   kapotteredmeny = eredmeny(jatekoslista, geplista)
+   varteredmeny = "jatekosvesztett"
+   if kapotteredmeny == varteredmeny:
+       print("Teszt sikeres")
+   else:
+       print("Teszt megbukott")
+
+def gepVesztetttobblappalTeszt():
+   jatekoslista = [6, 4, 8, ]
+   geplista =[6, 4, 3, 5]
+   kapotteredmeny = eredmeny(jatekoslista,geplista)
+   varteredmeny = "jatekosvesztett"
+   if kapotteredmeny==varteredmeny:
+       print("Teszt sikeres")
+   else:
+       print("Teszt megbukott")
+
+def dontetlenTeszt():
+   jatekoslista = [6, 4, 8,]
+   geplista =[6, 4, 8]
+   kapotteredmeny = eredmeny(jatekoslista,geplista)
+   varteredmeny = "döntetlen"
+   if kapotteredmeny==varteredmeny:
+       print("Teszt sikeres")
+   else:
+       print("Teszt megbukott")
 def tesztek():
     jatekosVesztettTeszt()
-    jatekosVesztettTeszt2()
-
+    jatekosVesztettkevesebbponttalTeszt()
+    jatekosVesztetttobblappalTeszt()
+    gepVesztettTeszt()
+    gepVesztettkevesebbponttalTeszt()
+    gepVesztetttobblappalTeszt()
+    dontetlenTeszt()
 tesztek()
