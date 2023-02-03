054b224782541e0ba0a96922f5a62e73ca6fd4fe
baalintka <ballint3@gmail.com>
2023-02-03 13:09:12 +0100

    jatekosteszt

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def eredmeny(Jatekospontok:[int], Geppontok:[int]):
         szoveg = "jatekosvesztett"
     elif Geppontok >21:
         szoveg = "gepvesztett"
+    elif Jatekospontok<Geppontok:
+        szoveg = "jatekosvesztett"
     return szoveg
 def pontszamitas(lapok: [int]):
     pontok=0
@@ -27,8 +29,17 @@ def jatekosVesztettTeszt():
        print("Teszt sikeres")
    else:
        print("Teszt megbukott")
-
+def jatekosVesztettTeszt2():
+   jatekoslista = [6, 4, 8,]
+   geplista =[6, 4, 11]
+   kapotteredmeny = eredmeny(jatekoslista,geplista)
+   varteredmeny = "jatekosvesztett"
+   if kapotteredmeny==varteredmeny:
+       print("Teszt sikeres2")
+   else:
+       print("Teszt megbukott2")
 def tesztek():
     jatekosVesztettTeszt()
+    jatekosVesztettTeszt2()
 
 tesztek()
